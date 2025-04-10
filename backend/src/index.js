51fab06cf1977b8135857465b2c03bd6e837a6f1
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2025-04-10 14:57:02 +0200

    connecting to database

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+const db = process.env.DATABASE;
+
+ const app = express()
+
+ app.listen(port,()=>{
+    console.log(`port running on ${port}`)
+ })
+
+ 
+ mongoose.connect(db)
+ .then(()=>{
+    console.log("data base connection succsesufly!")
+ }).catch((error)=>{
+    console.log(error)
+ })
